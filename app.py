e580d4fdfcf7b32278750cfe7d58f65aace3bbc7
Maulana Alamsyah <maulanalamsyah@gmail.com>
2024-07-21 16:31:04 +0700

    add html msg

--- app.py
+++ app.py
@@ -202,6 +202,19 @@ def sendMail(email, otp):
                   sender=('Aplikasi Preg-Fit', 'preg-fit@yopmail.com'), 
                   recipients=[email])
     msg.body = "Kode verifikasi OTP kamu adalah " + otp
+    msg.html = '''<div style="max-width: 600px; margin: 20px auto; background-color: #ffffff; border-radius: 8px; overflow: hidden;">
+        <div style="background-color: #fff;; padding: 0 20px; text-align: center; color: #7F91AA;">
+            <img style="display: block; margin: 0 auto; width: 80px;" src="https://i.ibb.co/vqMCTdS/Untitled-design-4.png" title="logo" alt="logo" data-bit="iit">
+            <h1 style="margin-top: 0;">Preg-Fit</h1>
+        </div>
+        <div style="height: 1px; background-color: #ecf0f1; margin: 20px 0;"></div>
+        <p>Hi Mom!<br/><br/>Ini dia kode rahasia kita. Jangan berikan kode ini ke siapa pun untuk alasan keamanan ya! <br/>Gunakan kode ini hanya untuk verifikasi akun <strong>{{app_name}}</strong> mom.<br/> Semoga hari mom menyenangkan yaa &#129392; &#129392;.</p><br> 
+      <center><span style="background: red; padding: 5px 15px 5px 15px; font-size: 20px; font-weight: bold; border-radius: 20px; color: white; background-color: #2c3e50;">{{otp}}</span></center>
+            <div style="height: 1px; background-color: #ecf0f1; margin: 20px 0;"></div>
+        <div style="text-align: center; padding: 20px; background-color: #ecf0f1;">
+            <p style="font-size: 14px; color: #03265B; margin: 0;">&copy; <strong>Preg-Fit</strong></p><div class="yj6qo"></div><div class="adL">
+        </div></div><div class="adL">
+    </div></div>'''
     mail.send(msg)
     return "OTP Sent to" + email
 
